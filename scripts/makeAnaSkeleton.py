1794aea96826687f194c7b2d1c6924f90a3df04d
Jose Palomino <palominogallo@Wilson2.local>
2015-09-21 16:36:02 +0900

    test

--- scripts/makeAnaSkeleton.py
+++ scripts/makeAnaSkeleton.py
@@ -7,7 +7,6 @@ def make_skeleton():
     workpath = sys.argv[1]
     dirname = sys.argv[2]
     classname = sys.argv[3]
-    os.system("mkdir -p %s/%s" % (workpath, dirname) )
     os.system("mkdir -p %s/%s/{src,include,make_histos,make_plots,rootfiles,plots}" % (workpath, dirname) )
     write_objects.write_rootclass( workpath+"/"+dirname+"/src/", classname )
     write_objects.write_rootheader( workpath+"/"+dirname+"/include/", classname )
