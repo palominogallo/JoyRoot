49c981bcb7cc280e38b97bd7abac48b8481867f9
Jose Palomino <palominogallo@Wilson2.local>
2015-09-21 16:34:24 +0900

    slight change on makeAnaSkeleton

--- scripts/makeAnaSkeleton.py
+++ scripts/makeAnaSkeleton.py
@@ -8,12 +8,7 @@ def make_skeleton():
     dirname = sys.argv[2]
     classname = sys.argv[3]
     os.system("mkdir -p %s/%s" % (workpath, dirname) )
-    os.system("mkdir -p %s/%s/src" % (workpath, dirname) )
-    os.system("mkdir -p %s/%s/include" % (workpath, dirname) )
-    os.system("mkdir -p %s/%s/make_histos" % (workpath, dirname) )
-    os.system("mkdir -p %s/%s/make_plots" % (workpath, dirname) )
-    os.system("mkdir -p %s/%s/rootfiles" % (workpath, dirname) )
-    os.system("mkdir -p %s/%s/plots" % (workpath, dirname) )
+    os.system("mkdir -p %s/%s/{src,include,make_histos,make_plots,rootfiles,plots}" % (workpath, dirname) )
     write_objects.write_rootclass( workpath+"/"+dirname+"/src/", classname )
     write_objects.write_rootheader( workpath+"/"+dirname+"/include/", classname )
     write_objects.write_histos( workpath+"/"+dirname+"/make_histos/", classname, "Basic" )
