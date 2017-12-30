07915f9b0a5243cfc5875ae9c2ab3d3ba81c7f5a
palominogallo <jose.palominogallo@stonybrook.edu>
2017-12-30 00:30:57 -0500

    preparing for cmake

--- setup.sh
+++ setup.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-if [ -z "$JOYROOTROOT" ]; then
-	echo " setting JOYROOT!! "
-	echo " "
-	export JOYROOTROOT=$HOME/Work/JoyRoot/
-	export DYLD_LIBRARY_PATH=$JOYROOTROOT/lib:$DYLD_LIBRARY_PATH #MAC OSX
-	#export LD_LIBRARY_PATH=$JOYROOTROOT/lib:$LD_LIBRARY_PATH
-else
-  echo " JOYROOT is already setup!!"
-  echo " "
+# Find the root of the building area.
+___joyroot_root() {
+    COUNT=10
+    while true; do
+			if [ -e ./lib -a -d ./lib -a -e ./lib/libJOYROOT.dylib ]; then
+				echo ${PWD}
+				return
+			fi
+			COUNT=$(expr ${COUNT} - 1)
+			if [ ${COUNT} -lt 1 ]; then
+				echo invalid-directory
+				return
+			fi
+			cd ..
+    done
+}
+
+export JOYROOT_ROOT
+JOYROOT_ROOT=$(___joyroot_root)
+unset -f ___joyroot_root
+
+if [ ${JOYROOT_ROOT} = "invalid-directory" ]; then
+    echo JOYROOT setup.sh must be sourced in your installation directory.
+    return
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    export DYLD_LIBRARY_PATH=$JOYROOT_ROOT/lib:${DYLD_LIBRARY_PATH}
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    export LD_LIBRARY_PATH=$JOYROOT_ROOT/lib:${LD_LIBRARY_PATH}
 fi
